fce54ee88b4f414a9e8c219a532158d6e2dcdc6e
Lucas Pereira de Oliveira <lucas.pereira_oli@hotmail.com>
2021-09-12 20:27:01 -0300

    Added bind favorite database and store

--- lib/src/app/app_module.dart
+++ lib/src/app/app_module.dart
@@ -1,6 +1,8 @@
 import 'package:dio/dio.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:starwars/src/core/core.dart';
+import 'package:starwars/src/features/favorites/controllers/favorite_controller.dart';
+import 'package:starwars/src/features/favorites/services/favorite_database.dart';
 
 import '../features/home/home_page.dart';
 
@@ -9,10 +11,18 @@ class AppModule extends Module {
   List<Bind<Object>> get binds => [
         Bind.singleton((i) => Dio()),
         Bind.singleton((i) => Api(client: i())),
+
+        // Favorite database
+
+        Bind((i) => FavoriteDatabase(), isLazy: false),
+        Bind((i) => FavoriteStore(favoriteDatabase: i()), export: true),
       ];
 
   @override
   List<ModularRoute> get routes => [
-        ChildRoute('/', child: (_, __) => const HomePage()),
+        ChildRoute(
+          '/',
+          child: (_, __) => HomePage(favoriteStore: Modular.get()),
+        ),
       ];
 }
